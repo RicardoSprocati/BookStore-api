bbe87e3042b46dccfa7c0b2784694d5fa9b16ad8
Ricardo Sprocati <ricardosprocatioliveira@outlook.com>
2025-07-24 10:55:38 -0300

    Revert "Merge pull request #18 from RicardoSprocati/deploy-render"
    This reverts commit 404ee734b4a2e789b88bfceb2ed7835170f91d2b, reversing
    changes made to d9de2bdfa2801739dc014dd9c3bbf131654d4176.

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "test-secret-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['bookstore-api-hfi0.onrender.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost").split(" ")
 
 # Application definition
 
